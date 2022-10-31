cde4a614408cc94a7aec817fba6655ebfdb7365d
PabloMikes <sefpaja@seznam.cz>
2022-11-01 00:11:31 +0100

    Update 1.8

--- res/js/main.js
+++ res/js/main.js
@@ -1,6 +1,10 @@
 const counter = document.getElementById("counter");
+
 const enemy = document.getElementById("enemy");
 const deadEnemy = document.getElementById("deadEnemy");
+const boss1 = document.getElementById("boss1");
+const deadBoss1 = document.getElementById("deadBoss1");
+
 const moneyCounter = document.getElementById("money");
 const sword = document.getElementById("sword");
 const MaxHealth = document.getElementById("MaxHealth");
@@ -50,6 +54,9 @@ const heal = document.getElementById("heal");
 const def = document.getElementById("def");
 const potionCounter = document.getElementById("potionCounter");
 const body = document.getElementsByTagName("body")[0];
+const enchant = document.getElementById("enchant");
+const moneyCounter2 = document.getElementById("moneyCounter2");
+const typeOfEnchantment = document.getElementById("typeOfEnchantment");
 
 let maxHp = 20;
 let hp = 20;
@@ -66,32 +73,66 @@ let up2 = 0;
 let price2 = 10;
 let damageOverTime = 0;
 let up3 = 0;
-let price3 = 300;
+let price3 = 500;
 let up4 = 0;
 let price4 = 1000;
 let up5 = 0;
 let price5 = 2000;
 let moneyPerSecond = 0;
 
+let p = 0;
+
+let poison = 0;
+let fire = 0;
+let doubleTap = 0;
+
+let bossHp = 1000;
+let bossMaxHp = 1000;
+let bossDmg = 10;
+let bossMoney = 500;
+
 attack.onclick = () => {
-  if (hp > 0) {
-    hp -= damage;
-    audio4.play();
-    mainChar.style.marginLeft = "55%";
+  if (deaths % 10 == 0 && deaths != 0) {
+    if (bossHp > 0) {
+      bossHp -= damage;
+      audio4.play();
+      mainChar.style.marginLeft = "55%";
+      setTimeout(() => {
+        mainChar.style.margin = "0 auto";
+      }, 100); //gay animace
+
+      boss1.style.transform = "scale(0.9)";
+      counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${bossHp}/${bossMaxHp}`;
+
+      setTimeout(() => {
+        boss1.style.transform = "scale(1)";
+      }, 100);
+    }
+
+    if (hp <= 0) {
+      boss1.style.display = "none";
+      deadBoss1.style.display = "block";
+    }
+  } else {
+    if (hp > 0) {
+      hp -= damage;
+      audio4.play();
+      mainChar.style.marginLeft = "55%";
+      setTimeout(() => {
+        mainChar.style.margin = "0 auto";
+      }, 100);
+    }
+    enemy.style.transform = "scale(0.9)";
+    counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
+
     setTimeout(() => {
-      mainChar.style.margin = "0 auto";
+      enemy.style.transform = "scale(1)";
     }, 100);
-  }
-  enemy.style.transform = "scale(0.9)";
-  counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
 
-  setTimeout(() => {
-    enemy.style.transform = "scale(1)";
-  }, 100);
-
-  if (hp <= 0) {
-    enemy.style.display = "none";
-    deadEnemy.style.display = "block";
+    if (hp <= 0) {
+      enemy.style.display = "none";
+      deadEnemy.style.display = "block";
+    }
   }
 };
 
@@ -117,7 +158,8 @@ sword.onclick = () => {
     price1 *= 1.3;
     price1 = Math.round(price1);
     sword.innerHTML = `Sharpness: ${up1} Gold: ${price1}`;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     damageCounter.innerHTML = `Damage: ${damage}`;
   }
 };
@@ -127,12 +169,13 @@ MaxHealth.onclick = () => {
     money -= price2;
     price2 *= 1.3;
     up2 += 1;
-    charHp += 10;
-    charMaxHp += 10;
+    charHp += 5;
+    charMaxHp += 5;
     hpCounter.innerHTML = `<img id="hp" src="./res/img/hp.png" alt="" />: ${charHp}/${charMaxHp}`;
     price2 = Math.round(price2);
     MaxHealth.innerHTML = `BUFF: ${up2} Gold: ${price2}`;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
   }
 };
 
@@ -140,7 +183,8 @@ potion.onclick = () => {
   if (money >= price3) {
     money -= price3;
     up3 += 1;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     price3 = Math.round(price3);
     potion.innerHTML = `Poťáček: ${up3} Gold: ${price3}`;
     potionCounter.innerHTML = `${up3}`;
@@ -152,29 +196,32 @@ club.onclick = () => {
     damage += 20;
     club.style.display = "none";
     money -= 100;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     damageCounter.innerHTML = `Damage: ${damage}`;
     description.style.display = "none";
   }
 };
 
 spear.onclick = () => {
-  if (money >= 1000 && deaths >= 15) {
+  if (money >= 1000 && deaths >= 25) {
     damage += 50;
     spear.style.display = "none";
     money -= 1000;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     damageCounter.innerHTML = `Damage: ${damage}`;
     description2.style.display = "none";
   }
 };
 
 mace.onclick = () => {
-  if (money >= 10000 && deaths >= 45) {
+  if (money >= 10000 && deaths >= 50) {
     damage += 100;
     mace.style.display = "none";
     money -= 10000;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     damageCounter.innerHTML = `Damage: ${damage}`;
     description3.style.display = "none";
   }
@@ -185,7 +232,8 @@ swordd.onclick = () => {
     damage += 200;
     swordd.style.display = "none";
     money -= 30000;
-    moneyCounter.innerHTML = `Money: ${money}`;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
     damageCounter.innerHTML = `Damage: ${damage}`;
     description4.style.display = "none";
   }
@@ -236,91 +284,187 @@ menuButton.onclick = () => {
     items.style.display = "none";
     verze.style.display = "none";
 
-    const interval2 = setInterval(() => {
-      if (hp <= 0) {
-        enemy.style.display = "none";
-        deadEnemy.style.display = "block";
-        audio5.play();
+    if (deaths % 10 == 0 && deaths != 0) {
+      boss1.style.display = "block";
+      enemy.style.display = "none";
+      counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${bossHp}/${bossMaxHp}`;
 
-        hp = Math.min(0);
-        money += maxMoney;
-        earnings.style.display = "block";
-        earnings.innerHTML = `+${maxMoney}G`;
-        enemyDamage += 0.5; //enemy damage scaling
-
-        setTimeout(() => {
-          earnings.style.display = "none";
-        }, 1000);
+      const interval4 = setInterval(() => {
+        boss1.style.margin = "0";
+        boss1.style.marginTop = "10%";
+        charHp -= bossDmg;
+        audio6.play();
+        hpCounter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${charHp}/${charMaxHp}`;
 
         setTimeout(() => {
+          boss1.style.margin = "0 auto";
+          boss1.style.marginTop = "10%";
+        }, 100); //gay animace
+      }, 3000);
 
-          maxMoney *= 1.3; //money scaling
-          maxMoney = Math.round(maxMoney);
-          moneyCounter.innerHTML = `Money: ${money}`;
-          maxHp *= 1.2; //hp scaling
-          maxHp = Math.round(maxHp);
-  
-          charHp -= charHp;
-          charHp += charMaxHp;
+      const interval3 = setInterval(() => {
+        if (bossHp <= 0) {
+          boss1.style.display = "none";
+          deadBoss1.style.display = "block";
+          audio5.play();
 
-          deaths++;
-          deathCounter.innerHTML = `Kills: ${deaths}`;
-          hp += maxHp;
-          hp = Math.max(maxHp);
-          counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
-          enemy.style.display = "block";
-          deadEnemy.style.display = "none";
+          money += bossMoney;
+          earnings.style.display = "block";
+          earnings.innerHTML = `+${bossMoney}G`;
+          bossDmg += 10; //boss damage scaling
 
+          setTimeout(() => {
+            earnings.style.display = "none";
+          }, 1000);
+
+          setTimeout(() => {
+            bossMoney += 1000; //bossmoney scaling
+            moneyCounter.innerHTML = `Gold: ${money}`;
+            moneyCounter2.innerHTML = `Gold: ${money}`;
+            bossMaxHp += 1000; //bosshp scaling
+
+            charHp -= charHp;
+            charHp += charMaxHp;
+
+            deaths++;
+            deathCounter.innerHTML = `Kills: ${deaths}`;
+            bossHp += bossMaxHp;
+            bossHp = Math.max(bossMaxHp);
+            counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
+            enemy.style.display = "block";
+            deadBoss1.style.display = "none";
+
+            main.style.display = "none";
+            menuButton.innerHTML = `Battle!`;
+            body.style.backgroundImage = "url(./res/img/background.jpg)";
+            mesto.style.display = "block";
+            items.style.display = "none";
+            menuButton.style.display = "block";
+            verze.style.display = "block";
+          }, 1000);
+          clearInterval(interval3);
+          clearInterval(interval4);
+        } else if (charHp <= 0) {
           main.style.display = "none";
+          goInside.style.display = "none";
           menuButton.innerHTML = `Battle!`;
           body.style.backgroundImage = "url(./res/img/background.jpg)";
           mesto.style.display = "block";
-          items.style.display = "none";
+          musicButton.style.display = "block";
           menuButton.style.display = "block";
-          verze.style.display = "block";
-        }, 1000);
-        clearInterval(interval);
-        clearInterval(interval2);
-      } else if (charHp <= 0) {
-        main.style.display = "none";
-        goInside.style.display = "none";
-        menuButton.innerHTML = `Battle!`;
-        body.style.backgroundImage = "url(./res/img/background.jpg)";
-        mesto.style.display = "block";
-        musicButton.style.display = "block";
-        menuButton.style.display = "block";
-        backButton.style.display = "none";
-        nextUpdate.style.display = "none";
-        shop.style.display = "block";
-        hospoda.style.display = "block";
-        hospoda.style.marginTop = "0";
-        backButton2.style.display = "none";
-        items.style.display = "none";
-        kostelButton.style.display = "block";
+          backButton.style.display = "none";
+          nextUpdate.style.display = "none";
+          shop.style.display = "block";
+          hospoda.style.display = "block";
+          hospoda.style.marginTop = "0";
+          backButton2.style.display = "none";
+          items.style.display = "none";
+          kostelButton.style.display = "block";
 
-        charHp -= charHp;
-        charHp += charMaxHp;
+          charHp -= charHp;
+          charHp += charMaxHp;
+          hpCounter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${charHp}/${charMaxHp}`;
 
-        hp -= hp;
-        hp += maxHp;
+          bossHp -= bossHp;
+          bossHp += bossMaxHp;
+          counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${bossHp}/${bossMaxHp}`;
 
-        clearInterval(interval);
-        clearInterval(interval2);
-      }
-    }, 100);
+          hp -= hp;
+          hp += maxHp;
+          counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
 
-    const interval = setInterval(() => {
-      enemy.style.margin = "0";
-      enemy.style.marginTop = "10%";
-      charHp -= enemyDamage;
-      audio6.play();
-      hpCounter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${charHp}/${charMaxHp}`;
+          clearInterval(interval);
+          clearInterval(interval2);
+          clearInterval(interval3);
+          clearInterval(interval4);
+        }
+      }, 100);
+    } else {
+      const interval2 = setInterval(() => {
+        if (hp <= 0) {
+          enemy.style.display = "none";
+          deadEnemy.style.display = "block";
+          audio5.play();
 
-      setTimeout(() => {
-        enemy.style.margin = "0 auto";
+          money += maxMoney;
+          earnings.style.display = "block";
+          earnings.innerHTML = `+${maxMoney}G`;
+          enemyDamage += 0.5; //enemy damage scaling
+
+          setTimeout(() => {
+            earnings.style.display = "none";
+          }, 1000);
+
+          setTimeout(() => {
+            maxMoney *= 1.3; //money scaling
+            maxMoney = Math.round(maxMoney);
+            moneyCounter.innerHTML = `Gold: ${money}`;
+            moneyCounter2.innerHTML = `Gold: ${money}`;
+            maxHp *= 1.2; //hp scaling
+            maxHp = Math.round(maxHp);
+
+            charHp -= charHp;
+            charHp += charMaxHp;
+
+            deaths++;
+            deathCounter.innerHTML = `Kills: ${deaths}`;
+            hp += maxHp;
+            hp = Math.max(maxHp);
+            counter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${hp}/${maxHp}`;
+            enemy.style.display = "block";
+            deadEnemy.style.display = "none";
+
+            main.style.display = "none";
+            menuButton.innerHTML = `Battle!`;
+            body.style.backgroundImage = "url(./res/img/background.jpg)";
+            mesto.style.display = "block";
+            items.style.display = "none";
+            menuButton.style.display = "block";
+            verze.style.display = "block";
+          }, 1000);
+          clearInterval(interval);
+          clearInterval(interval2);
+        } else if (charHp <= 0) {
+          main.style.display = "none";
+          goInside.style.display = "none";
+          menuButton.innerHTML = `Battle!`;
+          body.style.backgroundImage = "url(./res/img/background.jpg)";
+          mesto.style.display = "block";
+          musicButton.style.display = "block";
+          menuButton.style.display = "block";
+          backButton.style.display = "none";
+          nextUpdate.style.display = "none";
+          shop.style.display = "block";
+          hospoda.style.display = "block";
+          hospoda.style.marginTop = "0";
+          backButton2.style.display = "none";
+          items.style.display = "none";
+          kostelButton.style.display = "block";
+
+          charHp -= charHp;
+          charHp += charMaxHp;
+
+          hp -= hp;
+          hp += maxHp;
+
+          clearInterval(interval);
+          clearInterval(interval2);
+        }
+      }, 100);
+
+      const interval = setInterval(() => {
+        enemy.style.margin = "0";
         enemy.style.marginTop = "10%";
-      }, 100); //gay animace
-    }, 1500);
+        charHp -= enemyDamage;
+        audio6.play();
+        hpCounter.innerHTML = `<img src="./res/img/hp.png" alt="">: ${charHp}/${charMaxHp}`;
+
+        setTimeout(() => {
+          enemy.style.margin = "0 auto";
+          enemy.style.marginTop = "10%";
+        }, 100); //gay animace
+      }, 1500);
+    }
   }
 };
 
@@ -397,6 +541,8 @@ goInside.onclick = () => {
   goInside.style.display = "none";
   body.style.backgroundImage = "url(./res/img/insideKostel.jpg)";
   menuButton.style.display = "none";
+  enchant.style.display = "block";
+  moneyCounter2.style.display = "block";
 };
 
 goBack.onclick = () => {
@@ -406,4 +552,43 @@ goBack.onclick = () => {
   goInside.style.display = "block";
   body.style.backgroundImage = "url(./res/img/kostel.png)";
   menuButton.style.display = "block";
+  enchant.style.display = "none";
+  moneyCounter2.style.display = "none";
 };
+
+/*enchant.onclick = () => {
+  if (money >= 5000) {
+    money -= 5000;
+    moneyCounter.innerHTML = `Gold: ${money}`;
+    moneyCounter2.innerHTML = `Gold: ${money}`;
+    let random = Math.floor(Math.random() * 10);
+    if (random == 1) {
+      poison++;
+      typeOfEnchantment.innerHTML = `+ Poison`;
+      typeOfEnchantment.style.display = "block";
+      setTimeout(() => {
+        typeOfEnchantment.style.display = "none";
+      }, 2000);
+    } else if (random == 3) {
+      doubleTap++;
+      typeOfEnchantment.innerHTML = `+ Double Tap`;
+      typeOfEnchantment.style.display = "block";
+      setTimeout(() => {
+        typeOfEnchantment.style.display = "none";
+      }, 2000);
+    } else if (random == 8) {
+      fire++;
+      typeOfEnchantment.innerHTML = `+ Fire`;
+      typeOfEnchantment.style.display = "block";
+      setTimeout(() => {
+        typeOfEnchantment.style.display = "none";
+      }, 2000);
+    } else {
+      typeOfEnchantment.innerHTML = `Unlucky :/`;
+      setTimeout(() => {
+        typeOfEnchantment.style.display = "none";
+      }, 2000);
+    }
+  }
+};
+*/
